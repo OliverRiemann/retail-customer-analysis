e6057da7a106ebb4c4409022fca12ec917b11818
OliverRiemann <145388151+OliverRiemann@users.noreply.github.com>
2025-11-28 16:34:22 -0600

    Modularized scripts and save the lr model

--- etl.py
+++ etl.py
@@ -1,29 +1,34 @@
 from pyspark.sql import SparkSession
+from pyspark.sql import DataFrame
 from pyspark.sql.functions import col
 from pyspark.sql.functions import max, datediff, sum as _sum, count as _count
 from pyspark.sql.functions import lit
 
 spark = SparkSession.builder.getOrCreate()
 
+def load_data(input_path: str) -> DataFrame:
+    return spark.read.csv(input_path, header=True, inferSchema=True)
 
-
-def load_transform(data: str):
-    df = spark.read.csv(data, header=True, inferSchema=True)
- 
+def preprocess_data(df: DataFrame) -> DataFrame:
     df_clean = df.dropna(subset=["InvoiceNo", "CustomerID", "InvoiceDate", "UnitPrice", "Quantity"])
 
     df_clean = df_clean.filter(col("Quantity") > 0)
 
     df_clean = df_clean.dropDuplicates()
 
-    snapshot_date = df_clean.agg(max("InvoiceDate")).collect()[0][0]
+    return df_clean
 
-    rfm = df_clean.groupBy("CustomerID").agg(
-    datediff(lit(snapshot_date), max("InvoiceDate")).alias("Recency"),
-    _count("InvoiceNo").alias("Frequency"),
-    _sum(col("UnitPrice") * col("Quantity")).alias("Monetary")
+def rfm_calculation(df: DataFrame) -> DataFrame:
+    snapshot_date = df.agg(max("InvoiceDate")).collect()[0][0]
+
+    rfm = df.groupBy("CustomerID").agg(
+        datediff(lit(snapshot_date), max("InvoiceDate")).alias("Recency"),
+        _count("InvoiceNo").alias("Frequency"),
+        _sum(col("UnitPrice") * col("Quantity")).alias("Monetary")
     )
 
     return rfm
-    
-load_transform('online_retail.csv').show()
+
+df = load_data("online_retail.csv")
+df_clean = preprocess_data(df)
+rfm = rfm_calculation(df_clean)
--- lr_model.py
+++ lr_model.py
@@ -2,10 +2,11 @@ from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.classification import LogisticRegression
 from pyspark.ml import Pipeline
 from pyspark.sql import DataFrame
+from pyspark.ml.evaluation import BinaryClassificationEvaluator, MulticlassClassificationEvaluator
 
 # Create churn label (example: customers with no purchases in last N days)
-def build_rfm(data: DataFrame):
-    rfm = data.withColumn("Churn", (data["Recency"] > 90).cast("integer"))
+def lr_model(rfm_data: DataFrame, churn_recency: int = 90, model_path: str = "models/churn_lr"):
+    rfm = rfm_data.withColumn("Churn", (rfm_data["Recency"] > churn_recency).cast("integer"))
 
     features = ["Recency", "Frequency", "Monetary"]
     assembler = VectorAssembler(inputCols=features, outputCol="features")
@@ -17,8 +18,15 @@ def build_rfm(data: DataFrame):
     train, test = rfm.randomSplit([0.8, 0.2])
     model = pipeline.fit(train)
 
-    predictions = model.transform(test)
-    predictions.select("Churn", "prediction", "probability").show(10)
+    model.write().overwrite().save(model_path)
 
-    return rfm
+     # Evaluate
+    preds = model.transform(test)
+    roc_auc = BinaryClassificationEvaluator(labelCol="Churn", metricName="areaUnderROC").evaluate(preds)
+    pr_auc = BinaryClassificationEvaluator(labelCol="Churn", metricName="areaUnderPR").evaluate(preds)
+    accuracy = MulticlassClassificationEvaluator(labelCol="Churn", metricName="accuracy").evaluate(preds)
+    print(f"ROC AUC: {roc_auc}, PR AUC: {pr_auc}, Accuracy: {accuracy}")
+
+    return model
+
 
